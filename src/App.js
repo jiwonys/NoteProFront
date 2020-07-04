133030dac328b8c3beb33ff11e547aa07fdb4885
jiwonys <44040080+jiwonys@users.noreply.github.com>
2020-07-04 13:37:13 -0700

    git inital commit for NotePro Frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to NotePro
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:8080/createSticky/1/blue/1/2/thisisfirst"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create a Sticky
+        </a>
+        <a
+          className="App-link"
+          href="http://localhost:8080/load"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Load Sticky
+        </a>
+        <a
+          className="App-link"
+          href="http://localhost:8080/deleteAll"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Delete All Stickies
+        </a>
+        <a
+          className="App-link"
+          href="http://localhost:8080/save"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Save Stickies
         </a>
       </header>
     </div>
